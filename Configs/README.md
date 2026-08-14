f9d94b15b2a9649d6005775aa65b3ccace644af2
arun <arun.kr.reddy@gmail.com>
2026-08-14 12:56:17 +0000

    - remove WinMerge config - Winget.bat: remove redundant installations - MPV configs: fix brightness & double click pause issue - CSGO config: disable prediction

--- Configs/README.md
+++ Configs/README.md
@@ -5,6 +5,5 @@
   | WSL config         | `.wslconfig`              | `<User>\.wslconfig`                |
   | MPV config & binds | `mpv.conf` & `input.conf` | `<User>\AppData\Roaming\mpv.net`   |
   | CS2 autoexec       | `autoexec.cfg`            | `<CS2>\game\csgo\cfg\autoexec.cfg` |
-  | winmerge settings  | `WinMerge.ini`            | `%APPDATA%\WinMerge\`              |
   | winget commands    | `Winget.bat`              | run as admin                       |
   | opencode rules     | `AGENTS.md`               | `~/.config/opencode/AGENTS.md`     |
--- Configs/Winget.bat
+++ Configs/Winget.bat
@@ -8,7 +8,6 @@ winget install -e --id=qBittorrent.qBittorrent
 REM ============================ Utilities ============================
 winget install -e --id=7zip.7zip
 winget install -e --id=TheDocumentFoundation.LibreOffice
-winget install -e --id Flow-Launcher.Flow-Launcher
 winget install -e --id=xanderfrangos.twinkletray
 winget install -e --id Microsoft.WindowsTerminal
 winget install MusicBee
@@ -16,19 +15,15 @@ winget install MusicBee
 REM ============================ Coding ============================
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Git.Git
-winget install -e --id DEVCOM.JetBrainsMonoNerdFont
-winget install -e --id WinMerge.WinMerge
 
 REM ============================ Media ============================
 winget install -e --id=mpv.net
-winget install -e --id=HandBrake.HandBrake
 winget install -e --id=MoritzBunkus.MKVToolNix
 winget install -e --id=MediaArea.MediaInfo.GUI
 winget install -e --id jely2002.youtube-dl-gui
 
 REM ============================ Gaming & Hardware ============================
 winget install -e --id=Valve.Steam
-winget install -e --id=RazerInc.RazerInstaller.Synapse4
 
 REM ============================ Windows Settings ============================
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
@@ -41,3 +36,8 @@ REM https://winstall.app/
 REM winget install -e --id=Microsoft.VisualStudio.Community
 REM "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify --installPath "C:\Program Files\Microsoft Visual Studio\18\Community" --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended
 REM winget install -e --id=Nvidia.CUDA
+REM winget install -e --id Flow-Launcher.Flow-Launcher
+REM winget install -e --id DEVCOM.JetBrainsMonoNerdFont
+REM winget install -e --id WinMerge.WinMerge
+REM winget install -e --id=RazerInc.RazerInstaller.Synapse4
+REM winget install -e --id=HandBrake.HandBrake
